f0c6aeadba7fa155af156d94db2b18b38efa432f
Rafael Dias <rafael.cdc97@gmail.com>
2022-03-20 21:45:24 -0300

    mobile modal filter

--- android/app/src/main/kotlin/com/example/myfirstapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/myfirstapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myfirstapp
+package com.faeldiaz.calcgastocalorico
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -38,9 +39,11 @@ class _MyHomePageState extends State<MyHomePage> {
   final altura = TextEditingController();
   final idade = TextEditingController();
 
-
-  double _calculeGastoCalorico () {
-    double valor = (66 + (13.7 * double.parse(peso.text)) + (5.0 * double.parse(altura.text)) - (6.8 * int.parse(idade.text)));
+  double _calculeGastoCalorico() {
+    double valor = (66 +
+        (13.7 * double.parse(peso.text)) +
+        (5.0 * double.parse(altura.text)) -
+        (6.8 * int.parse(idade.text)));
     setState(() {
       _resultado = 'Gasto Calorico: ' + valor.toString() + 'KCal';
     });
@@ -52,6 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
+
   void _changeName() {
     setState(() {
       _name = myController.text;
@@ -76,94 +80,92 @@ class _MyHomePageState extends State<MyHomePage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               const Padding(
-                padding: EdgeInsets.fromLTRB(0, 50, 0, 50),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.all(Radius.circular(100)),
-                  child: Image(
-                    image: NetworkImage('https://amorpelacomida.com.br/wp-content/uploads/2016/08/comida-saudavel-150x150.jpg'),
-                    width: 150,
-                    height: 150,
-                  ),
-                )
-              ),
+                  padding: EdgeInsets.fromLTRB(0, 50, 0, 50),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(100)),
+                    child: Image(
+                      image: NetworkImage(
+                          'https://amorpelacomida.com.br/wp-content/uploads/2016/08/comida-saudavel-150x150.jpg'),
+                      width: 150,
+                      height: 150,
+                    ),
+                  )),
               const Align(
                 alignment: Alignment.centerLeft,
                 child: Padding(
-                    padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                    child: Text(
-                    'Peso (KG)',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.italic,
-                    )
-                  ),
+                  padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                  child: Text('Peso (KG)',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
+                      )),
                 ),
               ),
               Align(
                 alignment: Alignment.centerLeft,
                 child: Padding(
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 40),
-                    child: TextField(
-                      controller: peso,
-                    ),
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 40),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
+                    controller: peso,
+                  ),
                 ),
               ),
               const Align(
                 alignment: Alignment.centerLeft,
                 child: Padding(
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                    child: Text(
-                    'Altura (cm)',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.italic,
-                    )
-                  ),
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: Text('Altura (cm)',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
+                      )),
                 ),
               ),
               Align(
                 alignment: Alignment.centerLeft,
                 child: Padding(
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 40),
-                    child: TextField(
-                      controller: altura,
-                    ),
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 40),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
+                    controller: altura,
+                  ),
                 ),
               ),
               const Align(
                 alignment: Alignment.centerLeft,
                 child: Padding(
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                    child: Text(
-                    'Idade',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.italic,
-                    )
-                  ),
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: Text('Idade',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
+                      )),
                 ),
               ),
               Align(
                 alignment: Alignment.centerLeft,
                 child: Padding(
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 60),
-                    child: TextField(
-                      controller: idade,
-                    ),
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 60),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
+                    controller: idade,
+                  ),
                 ),
               ),
               Padding(
                 padding: EdgeInsets.fromLTRB(0, 0, 0, 60),
                 child: Text(_resultado),
               ),
-              // TextButton(
-              //   style: ButtonStyle(
-              //     foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-              //     backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
-              //   ),
-              //   onPressed: _calculeGastoCalorico,
-              //   child: Text('Calcular'),
-              // ),
             ],
           ),
         ),
